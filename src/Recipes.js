5274ed3e82b3f2b1cd597259b4b1b0ed1bacb09d
Richard George Lin <richard@Arcadia.localdomain>
2022-04-08 19:46:29 -0400

    eslint

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,8 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-key */
 export function Recipes(props) {
 	return (
 		<li>
 			<div style="float: left">
-				<img src={props.recipe_picture} alt="recipe picture"></img>
+				<img src={props.recipe_picture} alt="recipe"></img>
 			</div>
 			<div>
 				<b>Recipe: {props.recipe_title} </b>
