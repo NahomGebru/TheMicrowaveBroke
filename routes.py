7ff6701f0aac941a20f95d391c27bd0add3075d9
DellFair <rfair3@student.gsu.edu>
2022-04-08 21:15:57 -0400

    Added hose and port to routes.py

--- routes.py
+++ routes.py
@@ -261,5 +261,7 @@ def logout():
 
 if __name__ == "__main__":
     app.run(
-        debug=True,
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", 8080)),
+        debug=True
     )
